7e63f44b935e8eab7b9be1196f79939148461c51
Alonso <alonsoarredondoamador@gmail.com>
2026-05-29 01:48:20 +0200

    p6 exam3

--- src/test/java/com/uma/example/springuma/integration/MedicoControllerMockMvcIT.java
+++ src/test/java/com/uma/example/springuma/integration/MedicoControllerMockMvcIT.java
@@ -24,20 +24,34 @@ public class MedicoControllerMockMvcIT extends AbstractIntegration {
 
     private Medico medico;
 
-    @BeforeEach
-    void setUp() {
-        medico = new Medico();
-        medico.setId(1L);
-        medico.setDni("835");
-        medico.setNombre("Miguel");
-        medico.setEspecialidad("Ginecologia");
-    }
+    @Test
+    public void testCRUDEmpleado() throws Exception {
+        Medico medico = new Medico();
+        medico.setDni("12345678A");
+        medico.setNombre("Dr. House");
+        medico.setEspecialidad("Diagnóstico");
 
-    private void crearMedico(Medico medico) throws Exception {
-        this.mockMvc.perform(post("/medico")
-                .contentType("application/json")
-                .content(objectMapper.writeValueAsString(medico)))
+        // 1. Crear Médico
+        mockMvc.perform(post("/medico")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(medico)))
                 .andExpect(status().isCreated());
-    }
 
+        // 2. Get por ID
+        mockMvc.perform(get("/medico/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombre").value("Dr. House"));
+
+        // 3. Actualizar
+        medico.setId(1L);
+        medico.setNombre("Dr. Gregory House");
+        mockMvc.perform(put("/medico")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(medico)))
+                .andExpect(status().isNoContent());
+
+        // 4. Eliminar
+        mockMvc.perform(delete("/medico/1"))
+                .andExpect(status().isOk());
+    }
 }
